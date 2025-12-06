e3505285646911916ed72c9e35742e56a468ac6f
denisanemeth <denisanemeth542@gmail.com>
2025-12-06 16:35:54 +0200

    ROI +break-even functions

--- src/investment_func.c
+++ src/investment_func.c
@@ -16,3 +16,22 @@ double compound_with_contributions(double initial, double monthly, double rate, 
 double adjust_for_inflation(double amount,double inflaton_rate,int years){
     return amount/pow(1+inflaton_rate,years);
 }
+double compute_roi(double invested,double final){
+    return (final-invested)/invested;
+}
+int find_breakeven_years(double initial,double monthly,double rate){
+    double amount=initial;
+    double invested=initial;
+    double monthly_rate=pow(1+rate,1.0/12)-1;
+    for(int i=1;i<=50;i++){
+        for(int m=0;m<12;m++){
+            invested+=monthly;
+            amount=amount*(1+monthly_rate)+monthly;
+
+        }
+        if(amount>=invested){
+            return i;
+        }
+    }
+    return -1;
+}
--- src/investment_func.h
+++ src/investment_func.h
@@ -3,5 +3,6 @@
 double compound_interest(double principal,double rate,int years);
 double compound_with_contributions(double initial, double monthly, double rate, int years);
 double adjust_for_inflation(double amount,double inflation_rate,int years);
-
+double compute_roi(double invested,double final);
+int find_breakeven_years(double initial,double monthly,double rate);
 #endif
