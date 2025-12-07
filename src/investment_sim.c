4cb9ac49b1c5a43c8cd4f70331fb425fcc203eb2
denisanemeth <denisanemeth542@gmail.com>
2025-12-07 14:52:09 +0200

    recap,cleaning and testing math functions

--- src/investment_sim.c
+++ src/investment_sim.c
@@ -5,6 +5,10 @@
 int main() {
     double result = compound_interest(1000, 0.05, 10);
     double result2=compound_with_contributions(1000,100,0.05,10);
-    printf("result:%.2f,result 2:%.2f...\n",result,result2);
+    double ajustare=adjust_for_inflation(2000,0.02,5);
+    double roi=compute_roi(1500,2500);
+    int breakeven=find_breakeven_years(1000,100,0.05);
+    printf("result:%.2f,result 2:%.2f...\n,ajustare inflatie:%.2f/ncompute roi:%.2f /nfind breakeven y:%d",result,result2,ajustare,roi,breakeven);
     return 0;
+    
 }
